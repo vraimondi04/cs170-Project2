3d398114025447841bcaa09ecce03057fba0402b
vraimondi04 <42946121+vraimondi04@users.noreply.github.com>
2021-12-08 19:03:49 -0800

    update main to include initial user input and output and set up file variables

--- main.cpp
+++ main.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main() {
   
+  //user input and initial output
+  string fileName;
+  int userAlgorithm; 
+  cout << "Welcome to Vincent Raimondi's Feature Selection Algorithm." << endl << "Type in the name of the file to test: " << endl; 
+  cin >> fileName; 
+  cout << "Select the algorithm you want to run on this file:" << endl << "1) Forward Selection" << endl << "2) Backward Elimination" << endl; 
+  cin >> userAlgorithm; 
+	
+  //file variables to read in the small/large datasets; reference cs012 for reading and writing to files in c++
+	ifstream fin; 
+  fin.open(fileName);
+
+  //safety net if file cannot be opened; end program
+  if (!fin.is_open()) {
+    cout << "Error opening file." << endl; 
+    return -1; 
+  }
+  
   return 0;
 } 
