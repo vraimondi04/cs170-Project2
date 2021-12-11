e8aa5e1fa6b4b95f7432ab026659bb3569ca00e5
vraimondi04 <42946121+vraimondi04@users.noreply.github.com>
2021-12-10 16:45:34 -0800

    Implemented forwardSelection function; crossValidation function is currently a stub

--- main.cpp
+++ main.cpp
@@ -2,8 +2,80 @@
 #include <vector>
 #include <fstream> 
 #include <sstream>
+#include <algorithm> 
+#include <cmath> 
 
 using namespace std;
+
+void forwardSelection(vector<vector<long double>> trainingSet, int numFeatures) { 
+  //vector of features with highest accuracy; our final answer
+  vector<int> bestFeatures;
+  //vector of features with highest accuracy in the current traversal
+  vector<int> currBestFeatures;  
+  float accuracyVal = 0.0;
+  float highestAccuracy = 0.0;  
+    
+  for (int i = 1; i <= numFeatures - 1; ++i) {
+    vector<int> tempHighAccuracyVals;
+    float currentHighestA = 0.0; //current highest accuracy for this traversal
+
+    //referenced Project 2_full_briefing.pptx slide 26-27
+    for (int j = 1; j <= numFeatures; ++j) {
+      vector<vector<long double>> tempSet; 
+      vector<int> tempFeatures; 
+      tempSet.push_back(trainingSet.at(0)); 
+      tempFeatures = currBestFeatures; 
+            
+      for (int k = 0; k < tempFeatures.size(); ++k) { 
+        tempSet.push_back( trainingSet.at(tempFeatures.at(k)) ); 
+      } 
+                
+      //if the value of j is not found in the vector; assures that you won't compare features with themselves
+      if (find(tempFeatures.begin(), tempFeatures.end(), j) == tempFeatures.end()) {
+        //match the temporary set with the training set so they have corresponding rows to iterate through
+        tempSet.push_back(trainingSet.at(j)); 
+        tempFeatures.push_back(j); 
+        cout << "Using { ";
+
+        for (int i = 0; i < tempFeatures.size(); ++i) {
+          cout << tempFeatures.at(i) << " "; 
+        } 
+
+        //function call for cross-validation function to calculate the accuracy
+        accuracyVal = crossValidation(tempSet); 
+        cout << "} Accuracy: " << accuracyVal << "%" << endl;
+
+        if (accuracyVal > currentHighestA) { 
+          currentHighestA = accuracyVal; 
+          tempHighAccuracyVals = tempFeatures; 
+        }
+      }   
+    } 
+    currBestFeatures = tempHighAccuracyVals; 
+    cout << "The best feature subset is: "; 
+
+    for (int i = 0; i < currBestFeatures.size(); ++i) { 
+      cout << currBestFeatures.at(i) << " "; 
+    } 
+      cout << "with an accuracy of " << currentHighestA << "%" << endl << endl; 
+
+      //referenced Project 2_full_briefing.pptx slide 26-27
+      if (currentHighestA > highestAccuracy) { 
+        bestFeatures = currBestFeatures; 
+        highestAccuracy = currentHighestA; 
+        currBestFeatures = bestFeatures; 
+      }
+      else if (currentHighestA < highestAccuracy) { 
+        cout << "Oh Neptune! Accuracy value is decreasing. Checking in case of local maxima." << endl; 
+      }
+  }
+  cout << endl << "Finished search!" << endl; 
+  cout << "The final best feature subset is: "; 
+  for (int i = 0; i < bestFeatures.size(); ++i) { 
+    cout << bestFeatures.at(i) << " "; 
+  } 
+  cout << "with an accuracy of " << highestAccuracy << "%" << endl; 
+} 
 
 int main() {
   
